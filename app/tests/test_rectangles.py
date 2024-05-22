c45b08c7b2bc153b9b6dcc894af0d3fde3c77c47
Paulo Sandoval <ez2torta@gmail.com>
2024-05-22 19:24:27 -0400

    new test added that's failing lol

--- app/tests/test_rectangles.py
+++ app/tests/test_rectangles.py
@@ -6,6 +6,24 @@ def test_basic_rectangle_usage():
     assert rectangle
 
 
+def test_basic_solution_for_basic_rectangle():
+    big_rectangle = Rectangle(width=4, height=2)
+    small_rectangle = Rectangle(width=2, height=1)
+    problem = RectangleProblem(
+        big_rectangle=big_rectangle,
+        small_rectangle=small_rectangle,
+    )
+    problem_solution = problem.solve()
+
+    fixed_solution = [
+        Rectangle(width=2, height=1, position=RectanglePosition(x=0, y=0)),
+        Rectangle(width=2, height=1, position=RectanglePosition(x=0, y=1)),
+        Rectangle(width=2, height=1, position=RectanglePosition(x=1, y=0)),
+        Rectangle(width=2, height=1, position=RectanglePosition(x=1, y=1)),
+    ]
+    assert problem_solution == fixed_solution
+
+
 def test_fixed_solution_for_rectangle_problem():
     big_rectangle = Rectangle(width=5, height=3)
     small_rectangle = Rectangle(width=2, height=1)
