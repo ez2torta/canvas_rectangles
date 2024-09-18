67d7303af7115603d3e26266db9f22ad29af0192
Paulo Sandoval <ez2torta@gmail.com>
2024-09-18 02:06:45 -0300

    WIP ; aun nada pero hay canvas con divs

---
+++ static/backgroundanim.js
@@ -0,0 +1,307 @@
+const backgroundAnimationList = [
+    {
+        url: "piupc_82/SB00.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB01.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB02.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB03.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB04.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB05.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB06.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB07.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB08.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB09.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB10.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB11.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB12.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB13.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB14.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB15.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB16.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB17.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB18.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB19.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+    {
+        url: "piupc_82/SB20.PNG",
+        dx: 0,
+        dy: 0,
+        dWidth: 640,
+        dHeight: 480,
+        sx: 0,
+        sy: 0,
+        sWidth: 256,
+        sHeight: 256,
+    },
+];
+
+(function () {
+
+    var bg,
+        bgImage,
+        canvas;
+
+    function gameLoop() {
+        console.log("el gameloop")
+        window.requestAnimationFrame(gameLoop);
+
+        bg.update();
+        bg.render();
+    }
+
+    function backgroundAnimation(options) {
+
+        var that = {},
+            frameIndex = 0,
+            tickCount = 0,
+            ticksPerFrame = options.ticksPerFrame || 0;
+        // numberOfFrames = options.numberOfFrames || 1;
+        that.animationList = options.animationList;
+        that.numberOfFrames = that.animationList.length;
+        that.context = options.context;
+        that.sprite = options.sprite;
+
+        that.update = function () {
+
+            tickCount += 1;
+
+            if (tickCount > ticksPerFrame) {
+
+                tickCount = 0;
+
+                // If the current frame index is in range
+                if (frameIndex < numberOfFrames - 1) {
+                    // Go to the next frame
+                    frameIndex += 1;
+                } else {
+                    frameIndex = 0;
+                }
+            }
+        };
+
+        that.render = function () {
+
+            // Clear the canvas
+            that.context.clearRect(0, 0, 640, 480);
+            that.context.drawImage(bgImage, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+            bgImage.src = that.animationList[frameIndex];
+        };
+
+        return that;
+    }
+
+    // Get canvas
+    canvas = document.getElementById("bgCanvas");
+
+    // Create sprite sheet
+    bgImage = new Image();
+
+    // Create sprite
+    bg = backgroundAnimation({
+        context: canvas.getContext("2d"),
+        animationList: backgroundAnimationList,
+        numberOfFrames: 10,
+        ticksPerFrame: 4
+    });
+
+    // Load sprite sheet
+    bgImage.addEventListener("load", gameLoop);
+
+}());
--- static/spriteanim.js
+++ static/spriteanim.js
@@ -11,124 +11,212 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
-(function() {
+
+(function () {
 	// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 	// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
 	// requestAnimationFrame polyfill by Erik MÃ¶ller. fixes from Paul Irish and Tino Zijdel
 	// MIT license
+	console.log("Cargué")
 
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
+	for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+		window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+			|| window[vendors[x] + 'CancelRequestAnimationFrame'];
+	}
+
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = function (callback, element) {
+			console.log("request de frame")
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+				timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
+		};
+
+	if (!window.cancelAnimationFrame)
+		window.cancelAnimationFrame = function (id) {
+			console.log("cancel de frame")
+			clearTimeout(id);
+		};
 }());
 
+// (function () {
+
+// 	var coin,
+// 		coinImage,
+// 		canvas;
+
+// 	function gameLoop() {
+
+// 		window.requestAnimationFrame(gameLoop);
+
+// 		coin.update();
+// 		coin.render();
+// 	}
+
+// 	function sprite(options) {
+
+// 		var that = {},
+// 			frameIndex = 0,
+// 			tickCount = 0,
+// 			ticksPerFrame = options.ticksPerFrame || 0,
+// 			numberOfFrames = options.numberOfFrames || 1;
+
+// 		that.context = options.context;
+// 		that.width = options.width;
+// 		that.height = options.height;
+// 		that.image = options.image;
+
+// 		that.update = function () {
+
+// 			tickCount += 1;
+
+// 			if (tickCount > ticksPerFrame) {
+
+// 				tickCount = 0;
+
+// 				// If the current frame index is in range
+// 				if (frameIndex < numberOfFrames - 1) {
+// 					// Go to the next frame
+// 					frameIndex += 1;
+// 				} else {
+// 					frameIndex = 0;
+// 				}
+// 			}
+// 		};
+
+// 		that.render = function () {
+
+// 			// Clear the canvas
+// 			that.context.clearRect(0, 0, that.width, that.height);
+
+// 			// Draw the animation
+// 			that.context.drawImage(
+// 				that.image,
+// 				frameIndex * that.width / numberOfFrames,
+// 				0,
+// 				that.width / numberOfFrames,
+// 				that.height,
+// 				0,
+// 				0,
+// 				that.width / numberOfFrames,
+// 				that.height);
+// 		};
+
+// 		return that;
+// 	}
+
+// 	// Get canvas
+// 	canvas = document.getElementById("coinAnimation");
+// 	canvas.width = 100;
+// 	canvas.height = 100;
+
+// 	// Create sprite sheet
+// 	coinImage = new Image();
+
+// 	// Create sprite
+// 	coin = sprite({
+// 		context: canvas.getContext("2d"),
+// 		width: 1000,
+// 		height: 100,
+// 		image: coinImage,
+// 		numberOfFrames: 10,
+// 		ticksPerFrame: 4
+// 	});
+
+// 	// Load sprite sheet
+// 	coinImage.addEventListener("load", gameLoop);
+// 	coinImage.src = "images/coin-sprite-animation.png";
+
+// }());
+
+
+
 (function () {
-			
+
 	var coin,
 		coinImage,
-		canvas;					
+		canvas;
 
-	function gameLoop () {
-	
-	  window.requestAnimationFrame(gameLoop);
+	function gameLoop() {
+		console.log("el gameloop de spriteanim")
+		window.requestAnimationFrame(gameLoop);
 
-	  coin.update();
-	  coin.render();
+		coin.update();
+		coin.render();
 	}
-	
-	function sprite (options) {
-	
+
+	function spritesheetAnimation(options) {
+
 		var that = {},
 			frameIndex = 0,
 			tickCount = 0,
 			ticksPerFrame = options.ticksPerFrame || 0,
 			numberOfFrames = options.numberOfFrames || 1;
-		
-		that.context = options.context;
-		that.width = options.width;
-		that.height = options.height;
 		that.image = options.image;
-		
+		that.context = options.context;
+		that.sprite = options.sprite;
+
 		that.update = function () {
 
-            tickCount += 1;
+			tickCount += 1;
 
-            if (tickCount > ticksPerFrame) {
+			if (tickCount > ticksPerFrame) {
 
 				tickCount = 0;
-				
-                // If the current frame index is in range
-                if (frameIndex < numberOfFrames - 1) {	
-                    // Go to the next frame
-                    frameIndex += 1;
-                } else {
-                    frameIndex = 0;
-                }
-            }
-        };
-		
-		that.render = function () {
-		
-		  // Clear the canvas
-		  that.context.clearRect(0, 0, that.width, that.height);
-		  
-		  // Draw the animation
-		  that.context.drawImage(
-		    that.image,
-		    frameIndex * that.width / numberOfFrames,
-		    0,
-		    that.width / numberOfFrames,
-		    that.height,
-		    0,
-		    0,
-		    that.width / numberOfFrames,
-		    that.height);
+
+				// If the current frame index is in range
+				if (frameIndex < numberOfFrames - 1) {
+					// Go to the next frame
+					frameIndex += 1;
+				} else {
+					frameIndex = 0;
+				}
+			}
 		};
-		
+
+		that.render = function () {
+
+			// Clear the canvas
+			that.context.clearRect(0, 0, 640, 480);
+
+			// Draw the animation
+			that.context.drawImage(
+				that.image,
+				frameIndex * that.width / numberOfFrames,
+				0,
+				that.width / numberOfFrames,
+				that.height,
+				0,
+				0,
+				that.width / numberOfFrames,
+				that.height);
+		};
+
 		return that;
 	}
-	
+
 	// Get canvas
 	canvas = document.getElementById("coinAnimation");
-	canvas.width = 100;
-	canvas.height = 100;
-	
+
 	// Create sprite sheet
-	coinImage = new Image();	
-	
+	coinImage = new Image();
+
 	// Create sprite
-	coin = sprite({
+	coin = spritesheetAnimation({
 		context: canvas.getContext("2d"),
-		width: 1000,
-		height: 100,
 		image: coinImage,
 		numberOfFrames: 10,
 		ticksPerFrame: 4
 	});
-	
+
 	// Load sprite sheet
 	coinImage.addEventListener("load", gameLoop);
 	coinImage.src = "images/coin-sprite-animation.png";
 
-} ());
-
+}());
